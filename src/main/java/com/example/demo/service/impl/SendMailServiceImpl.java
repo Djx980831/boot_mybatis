f1d697fc9202b15f4354c9041c44d181dc885c80
Djx980831 <1530916212@qq.com>
2020-10-08 02:03:08 +0800

    add

--- src/main/java/com/example/demo/service/impl/SendMailServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SendMailServiceImpl.java
@@ -49,14 +49,6 @@ public class SendMailServiceImpl implements SendMailService {
 
     public boolean sendMail1() throws Exception {
         ArrayList<Person> allPersonList = mailMapper.getAllPerson();
-        //文本固定内容 todo
-        StringBuilder sb = new StringBuilder("新系统PLM的登录用户名是 本人域账号");
-        for (int i = 0; i < allPersonList.size(); i++) {
-            sb.append("，密码是：").append(RandomPassword.getSomeString());
-            mailText = sb.toString();
-            mailTo = allPersonList.get(i).getMail();
-            sb = null;
-        }
 
         Properties prop = new Properties();
         prop.setProperty("mail.host", mail_host);
@@ -64,22 +56,26 @@ public class SendMailServiceImpl implements SendMailService {
         prop.setProperty("mail.smtp.auth", "true");
         prop.setProperty("mail.smtp.port", "587");
 
-        // 使用JavaMail发送邮件的5个步骤
+        for (int i = 0; i < allPersonList.size(); i++) {
+            StringBuilder sb = new StringBuilder("新系统PLM的登录用户名是 本人域账号");
+            sb.append("，密码是：").append(RandomPassword.getSomeString());
+            mailText = sb.toString();
+            mailTo = allPersonList.get(i).getMail();
 
-        // 1、创建session
-        Session session = Session.getInstance(prop);
-        // 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
-        session.setDebug(true);
-        // 2、通过session得到transport对象
-        Transport ts = session.getTransport();
-        // 3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
-        ts.connect(mail_host,mailFrom, password_mailFrom);
-        // 4、创建邮件
-        Message message = createSimpleMail(session,mailFrom,mailTo,mailTittle,mailText);
-        // 5、发送邮件
-        ts.sendMessage(message, message.getAllRecipients());
-        ts.close();
-
+            // 1、创建session
+            Session session = Session.getInstance(prop);
+            // 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
+            session.setDebug(true);
+            // 2、通过session得到transport对象
+            Transport ts = session.getTransport();
+            // 3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
+            ts.connect(mail_host,mailFrom, password_mailFrom);
+            // 4、创建邮件
+            Message message = createSimpleMail(session,mailFrom,mailTo,mailTittle,mailText);
+            // 5、发送邮件
+            ts.sendMessage(message, message.getAllRecipients());
+            ts.close();
+        }
 
         return true;
     }
